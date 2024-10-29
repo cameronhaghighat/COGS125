80885b2e225ec10e0184ee552cde145499f5176f
Cameron Haghighat <cameronhaghighat@MacBook-Pro-83.attlocal.net>
2024-10-28 18:32:24 -0700

    idk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
 // src/App.js
 import React from 'react';
@@ -108,5 +86,4 @@ function Project({ title, description, link }) {
   );
 }
 
-export default App;
 export default App;
